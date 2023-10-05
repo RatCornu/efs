2e353de402e0fe0324af1d742ca1bc91b2943f09
Rat Cornu <balthazar.patiachvili@crans.org>
2023-10-06 01:59:35 +0200

    feat: partial completion of Stat structure

--- src/file.rs
+++ src/file.rs
@@ -2,7 +2,7 @@
 //!
 //! See [this Wikipedia page](https://en.wikipedia.org/wiki/Unix_file_types) for more informations.
 
-use crate::types::Dev;
+use crate::types::{Blkcnt, Blksize, Dev, Gid, Ino, Mode, Nlink, Off, Timespec, Uid};
 
 /// Enumeration of possible file types
 #[derive(Debug, Clone, Copy)]
@@ -30,8 +30,52 @@ pub enum Type {
     UnixSocket,
 }
 
-/// Main trait for all Unix files
-pub trait File {
+/// Minimal stat structure
+///
+/// More informations on [the POSIX definition](https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/sys_stat.h.html#tag_13_62)
+pub struct Stat {
     /// Device ID of device containing file
-    fn st_dev(&self) -> Dev;
+    pub st_dev: Dev,
+
+    /// File serial number
+    pub st_ino: Ino,
+
+    /// Mode of file
+    pub st_mode: Mode,
+
+    /// Number of hard links to the file
+    pub st_nlink: Nlink,
+
+    /// User ID of file
+    pub st_uid: Uid,
+
+    /// Group ID of file
+    pub st_gid: Gid,
+
+    /// Device ID (if file is character or block special)
+    pub st_rdev: Dev,
+
+    /// For regular files, the file size in bytes
+    ///
+    /// For symbolic links, the length in bytes of the pathname contained in the symbolic link.
+    pub st_size: Off,
+
+    /// Last data access timestamp
+    pub st_atim: Timespec,
+
+    /// Last data modification timestamp
+    pub st_mtim: Timespec,
+
+    /// Last file status change timestamp
+    pub st_ctim: Timespec,
+
+    /// A file system-specific preferred I/O block size for this object. In some file system types, this may vary from file to
+    /// file.
+    pub st_blksize: Blksize,
+
+    /// Number of blocks allocated for this object
+    pub st_blkcnt: Blkcnt,
 }
+
+/// Main trait for all Unix files
+pub trait File: Into<Stat> {}
--- src/types.rs
+++ src/types.rs
@@ -4,21 +4,69 @@ use derive_more::{Deref, DerefMut};
 
 /// Used for device IDs
 #[derive(Debug, Clone, Copy, Deref, DerefMut)]
-pub struct Dev(pub u32);
+pub struct Dev(pub u16);
 
-impl Dev {
-    /// Major device ID
-    pub fn major(&self) -> u32 {
-        todo!()
-    }
+// impl Dev {
+//     /// Major device ID
+//     pub fn major(&self) -> u16 {
+//         todo!()
+//     }
+//
+//     /// Minor device ID
+//     pub fn minor(&self) -> u16 {
+//         todo!()
+//     }
+//
+//     /// Create a device ID from a major and a minor device ID
+//     pub fn makedev(major: u16, minor: u16) -> Self {
+//         todo!()
+//     }
+// }
 
-    /// Minor device ID
-    pub fn minor(&self) -> u32 {
-        todo!()
-    }
+/// Used for file serial numbers
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Ino(pub u32);
 
-    /// Create a device ID from a major and a minor device ID
-    pub fn makedev(major: u32, minor: u32) -> Self {
-        todo!()
-    }
+/// Used for some file attributes
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Mode(pub u16);
+
+/// Used for link counts
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Nlink(pub u32);
+
+/// Used for user IDs
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Uid(pub u16);
+
+/// Used for group IDs
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Gid(pub u16);
+
+/// Used for file sizes
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Off(pub i32);
+
+/// Used for block sizes
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Blksize(pub i32);
+
+/// Used for file block counts
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Blkcnt(pub i32);
+
+/// Used for time in seconds
+#[derive(Debug, Clone, Copy, Deref, DerefMut)]
+pub struct Time(pub i32);
+
+/// Used for time in seconds
+///
+/// Times shall be given in seconds since the Epoch
+#[derive(Debug, Clone, Copy)]
+pub struct Timespec {
+    /// Seconds
+    pub tv_sec: Time,
+
+    /// Nanoseconds
+    pub tv_nsec: u32,
 }
