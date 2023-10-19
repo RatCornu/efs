369d51befa541be4053b1823ddd012a8817a8453
Rat Cornu <balthazar.patiachvili@crans.org>
2023-10-19 02:02:30 +0200

    feat: implement doubleSidedIterator for Components

--- src/fs.rs
+++ src/fs.rs
@@ -2,6 +2,8 @@
 
 use alloc::boxed::Box;
 
+use no_std_io::io::Result;
+
 use crate::file::{Directory, File};
 use crate::path::Path;
 
@@ -12,10 +14,13 @@ pub trait FileSystem {
 
     /// Performs a pathname resolution as described in [this POSIX definition](https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap04.html#tag_04_13).
     ///
-    /// Returns the file of this filesystem corresponding to the given [`Path`]
+    /// Returns the file of this filesystem corresponding to the given [`Path`].
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`NotFound`](no_std_io::io::ErrorKind) if the given path does not leed to an existing path.
     #[inline]
-    #[must_use]
-    fn pathname_resolution(&self, _path: &Path) -> Box<dyn File> {
+    fn pathname_resolution(&self, _path: &Path) -> Result<Box<dyn File>> {
         todo!()
     }
 }
--- src/path.rs
+++ src/path.rs
@@ -3,6 +3,7 @@
 use alloc::borrow::{Cow, ToOwned};
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
+use core::iter::FusedIterator;
 use core::str::FromStr;
 
 use once_cell::sync::Lazy;
@@ -33,7 +34,7 @@ impl<'path> UnixStr<'path> {
     #[inline]
     #[must_use]
     pub fn new(str: &'path str) -> Option<Self> {
-        if str.is_empty() || str.contains('\0') { None } else { Some(Self(Cow::from(str))) }
+        (!str.is_empty() && !str.contains('\0')).then_some(Self(Cow::from(str)))
     }
 
     /// Checks whether the inner string contains exactly two leading '/' characters
@@ -75,7 +76,7 @@ impl<'path> From<Component<'path>> for UnixStr<'path> {
     }
 }
 
-/// A slice of a path
+/// A slice of a path.
 ///
 /// It is represented by a string that is used to identify a file. It has optional beginning `/`, followed by zero or more filenames
 /// separated by `/`. A pathname can optionally contain one or more trailing `/`. Multiple successive `/` characters are considered
@@ -277,6 +278,9 @@ impl PartialEq for Path<'_> {
 
 impl Eq for Path<'_> {}
 
+/// Root directory.
+pub static ROOT: Lazy<Path> = Lazy::new(|| Path::from_str("/").unwrap_or_else(|_| unreachable!("ROOT is a non-empty path")));
+
 /// Component parsing works by a double-ended state machine; the cursors at the
 /// front and back of the path each keep track of what parts of the path have
 /// been consumed so far.
@@ -345,8 +349,10 @@ pub struct Components<'path> {
     /// It must be ensure that the given `path` is under a canonical form.
     path: &'path [u8],
 
-    /// Is the *original* path rooted ?
-    has_root: bool,
+    /// Starting dir of the path.
+    ///
+    /// If [`Some`](Option), can only contain [`RootDir`](Component), [`DoubleSlashRootDir`](Component) or [`CurDir`](Component).
+    start_dir: Option<Component<'path>>,
 
     /// Keeps track of what has been consumed on the left side of the path.
     front: State,
@@ -360,12 +366,40 @@ impl<'path> Components<'path> {
     #[inline]
     #[must_use]
     pub fn new(path: &'path Path<'path>) -> Self {
+        let bytes = path.name.0.as_bytes();
+
+        let start_dir = if path.as_unix_str().starts_with_two_slashes() {
+            Some(Component::DoubleSlashRootDir)
+        } else if path.is_absolute() {
+            Some(Component::RootDir)
+        } else {
+            let mut iter = bytes.iter();
+            match (iter.next(), iter.next()) {
+                (Some(&b'.'), None | Some(&b'/')) => Some(Component::CurDir),
+                _ => None,
+            }
+        };
         Self {
-            path: path.name.0.as_bytes(),
-            has_root: path.is_absolute(),
+            path: bytes,
+            start_dir,
             front: State::StartDir,
-            back: State::StartDir,
+            back: State::Body,
         }
+    }
+
+    /// Does the original path starts with [`RootDir`](Component) ?
+    fn has_root(&self) -> bool {
+        self.start_dir == Some(Component::RootDir)
+    }
+
+    /// Does the original path starts with [`DoubleSlashRootDir`](Component) ?
+    fn has_double_slash_root(&self) -> bool {
+        self.start_dir == Some(Component::DoubleSlashRootDir)
+    }
+
+    /// Does the original path starts with [`CurDir`](Component) ?
+    fn include_cur_dir(&self) -> bool {
+        self.start_dir == Some(Component::CurDir)
     }
 
     /// Is the iteration complete ?
@@ -375,17 +409,17 @@ impl<'path> Components<'path> {
         self.front == State::Done || self.back == State::Done
     }
 
-    /// Should the normalized path include a leading `.` ?
-    fn include_cur_dir(&self) -> bool {
-        if self.has_root {
-            return false;
-        };
-
-        let mut iter = self.path.iter();
-        match (iter.next(), iter.next()) {
-            (Some(&b'.'), None) => true,
-            (Some(&b'.'), Some(&byte)) => byte == b'/',
-            _ => false,
+    /// Number of bytes before the [`Path`]'s body
+    fn len_before_body(&self) -> usize {
+        if self.front == State::StartDir {
+            match self.start_dir {
+                None => 0,
+                Some(Component::RootDir | Component::CurDir) => 1,
+                Some(Component::DoubleSlashRootDir) => 2,
+                _ => unreachable!(),
+            }
+        } else {
+            0
         }
     }
 
@@ -404,6 +438,27 @@ impl<'path> Components<'path> {
         // SAFETY: `comp` is a valid substring since it is split on `/`
         (comp.len() + extra, Component::from_str(&unsafe { String::from_utf8_unchecked(comp.to_vec()) }).ok())
     }
+
+    /// Parse a component from the right, saying how many bytes to consume to remove it.
+    fn parse_next_component_back(&self) -> (usize, Option<Component<'path>>) {
+        let start = self.len_before_body();
+        let body = self
+            .path
+            .get(start..)
+            .unwrap_or_else(|| unreachable!("This function is only called with `self.back == State::Body`"));
+        let (extra, comp) = body.iter().rposition(|byte| byte == &b'/').map_or((0_usize, body), |idx| {
+            (
+                1_usize,
+                &self
+                    .path
+                    .get(start + idx + 1..)
+                    .unwrap_or_else(|| unreachable!("The index exists since it is returned by the find function")),
+            )
+        });
+
+        // SAFETY: `comp` is a valid substring since it is split on `/`
+        (comp.len() + extra, Component::from_str(&unsafe { String::from_utf8_unchecked(comp.to_vec()) }).ok())
+    }
 }
 
 impl<'path> Iterator for Components<'path> {
@@ -415,11 +470,11 @@ impl<'path> Iterator for Components<'path> {
             match self.front {
                 State::StartDir => {
                     self.front = State::Body;
-                    if self.path.starts_with(&[b'/', b'/']) {
+                    if self.has_double_slash_root() {
                         // SAFETY: `self.path` contains at least 2 element
                         self.path = unsafe { &self.path.get_unchecked(2..) };
                         return Some(Component::DoubleSlashRootDir);
-                    } else if self.path.starts_with(&[b'/']) {
+                    } else if self.has_root() {
                         // SAFETY: `self.path` contains at least 1 element
                         self.path = unsafe { &self.path.get_unchecked(1..) };
                         return Some(Component::RootDir);
@@ -448,8 +503,47 @@ impl<'path> Iterator for Components<'path> {
     }
 }
 
-/// Root directory
-pub static ROOT: Lazy<Path> = Lazy::new(|| Path::from_str("/").unwrap_or_else(|_| unreachable!("ROOT is a non-empty path")));
+impl<'path> DoubleEndedIterator for Components<'path> {
+    #[inline]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        while !self.is_finished() {
+            match self.back {
+                State::Body if self.path.len() > self.len_before_body() => {
+                    let (size, comp) = self.parse_next_component_back();
+                    // SAFETY: It is ensure in `parse_next_component_back` that `self.path` contains at least `size` characters
+                    self.path = unsafe { &self.path.get_unchecked(..self.path.len() - size) };
+                    if comp.is_some() {
+                        return comp;
+                    }
+                },
+                State::Body => {
+                    self.back = State::StartDir;
+                    if self.front == State::StartDir {
+                        if self.has_double_slash_root() {
+                            self.path = &[];
+                            return Some(Component::DoubleSlashRootDir);
+                        } else if self.has_root() {
+                            self.path = &[];
+                            return Some(Component::RootDir);
+                        } else if self.include_cur_dir() {
+                            self.path = &[];
+                            return Some(Component::CurDir);
+                        }
+                    }
+                },
+                State::StartDir => {
+                    self.back = State::Done;
+                    return None;
+                },
+                State::Done => unreachable!(),
+            }
+        }
+
+        None
+    }
+}
+
+impl FusedIterator for Components<'_> {}
 
 #[cfg(test)]
 mod test {
@@ -535,5 +629,84 @@ mod test {
         assert_eq!(components.next(), Some(Component::ParentDir));
         assert_eq!(components.next(), Some(Component::CurDir));
         assert_eq!(components.next(), None);
+    }
+
+    #[test]
+    fn path_components_back() {
+        let path = Path::from_str("/home/user/foo").unwrap();
+        let mut components = path.components();
+
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("foo").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("user").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("home").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::RootDir));
+        assert_eq!(components.next_back(), None);
+
+        let path = Path::from_str("./foo//../baz").unwrap();
+        let mut components = path.components();
+
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("baz").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::ParentDir));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("foo").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::CurDir));
+        assert_eq!(components.next_back(), None);
+
+        let path = Path::from_str("foo/bar///..").unwrap();
+        let mut components = path.components();
+
+        assert_eq!(components.next_back(), Some(Component::ParentDir));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("bar").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("foo").unwrap())));
+        assert_eq!(components.next_back(), None);
+
+        let path = Path::from_str("//home/foo/.././").unwrap();
+        let mut components = path.components();
+
+        assert_eq!(components.next_back(), Some(Component::CurDir));
+        assert_eq!(components.next_back(), Some(Component::ParentDir));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("foo").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("home").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::DoubleSlashRootDir));
+        assert_eq!(components.next_back(), None);
+    }
+
+    #[test]
+    fn path_components_double_side() {
+        let path = Path::from_str("/home/user/foo").unwrap();
+        let mut components = path.components();
+
+        assert_eq!(components.next(), Some(Component::RootDir));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("foo").unwrap())));
+        assert_eq!(components.next(), Some(Component::Normal(UnixStr::from_str("home").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("user").unwrap())));
+        assert_eq!(components.next_back(), None);
+        assert_eq!(components.next(), None);
+
+        let path = Path::from_str("./foo//../baz").unwrap();
+        let mut components = path.components();
+
+        assert_eq!(components.next(), Some(Component::CurDir));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("baz").unwrap())));
+        assert_eq!(components.next(), Some(Component::Normal(UnixStr::from_str("foo").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::ParentDir));
+        assert_eq!(components.next(), None);
+
+        let path = Path::from_str("foo/bar///..").unwrap();
+        let mut components = path.components();
+
+        assert_eq!(components.next_back(), Some(Component::ParentDir));
+        assert_eq!(components.next(), Some(Component::Normal(UnixStr::from_str("foo").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::Normal(UnixStr::from_str("bar").unwrap())));
+        assert_eq!(components.next(), None);
+
+        let path = Path::from_str("//home/foo/.././").unwrap();
+        let mut components = path.components();
+
+        assert_eq!(components.next(), Some(Component::DoubleSlashRootDir));
+        assert_eq!(components.next_back(), Some(Component::CurDir));
+        assert_eq!(components.next(), Some(Component::Normal(UnixStr::from_str("home").unwrap())));
+        assert_eq!(components.next_back(), Some(Component::ParentDir));
+        assert_eq!(components.next(), Some(Component::Normal(UnixStr::from_str("foo").unwrap())));
+        assert_eq!(components.next_back(), None);
     }
 }
